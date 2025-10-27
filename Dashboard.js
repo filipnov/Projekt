aee2d5dfa884baefd8ab2d9f9f33930093ea3724
filipnov <fifonovak2@gmail.com>
2025-10-27 17:13:25 +0100

    Updt Dash

--- Dashboard.js
+++ Dashboard.js
@@ -1,9 +1,10 @@
 // Dashboard.js
 import { useState } from "react";
-import { StyleSheet, Text, View, Image, Pressable } from "react-native";
+import { StyleSheet, Text, View, Image, Pressable, ScrollView } from "react-native";
 import { useNavigation, useRoute } from "@react-navigation/native";
 import logo from "./assets/logo.png";
 import account from "./assets/avatar.png";
+
 
 export default function Dashboard() {
   const navigation = useNavigation();
@@ -13,7 +14,7 @@ export default function Dashboard() {
   const { nick } = route.params || { nick: "User" };
 
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       {/* Top bar with logo, nickname, and avatar */}
       <View style={styles.topBar}>
         <Image source={logo} style={styles.topBar_img} />
@@ -21,11 +22,19 @@ export default function Dashboard() {
         <Image source={account} style={styles.topBar_img} />
       </View>
 
+      {/*Content container*/}
+      <View style={styles.contentContainer}>
+        {/*Left bar*/}
+        <View style={styles.leftBar}></View>
+        {/*Main content*/}
+        <View></View>
+      </View>
+
       {/* Back button */}
       <Pressable onPress={() => navigation.navigate("HomeScreen")}>
         <Text style={styles.backText}>Chod späť</Text>
       </Pressable>
-    </View>
+    </ScrollView>
   );
 }
 
@@ -33,16 +42,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+    height: "fit-content"
   },
   topBar: {
-    backgroundColor: "hsl(0, 0%, 88%)",
+    backgroundColor: "hsl(0, 0%, 95%)",
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-around",
     paddingBottom: 20,
     elevation: 10,
-    borderBottomLeftRadius: 25,
     borderBottomRightRadius: 25,
+    borderColor: "black",
+    borderWidth: 1
   },
   topBar_text: {
     marginTop: 50,
@@ -59,4 +70,13 @@ const styles = StyleSheet.create({
     color: "blue",
     margin: 20,
   },
+  leftBar: {
+    width: 200,
+    height: "100%",
+    borderColor: "black",
+    borderWidth: 1
+  },
+  contentContainer: {
+    height: 700
+  }
 });
